5372cc43d6a2800be084b3fc271bd03bcc1be80c
jake <jake.glee1122@gmail.com>
2025-07-25 13:33:32 -0700

    Co-authored-by: yepitsaname <yepitsaname@users.noreply.github.com>

--- README.md
+++ README.md
@@ -10,3 +10,23 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+---
+
+Project Specs
+
+You will be creating an application that will display a list of cocktails made with a searched ingredient. Your application should do the following at a bare minimum:
+
+Render a default list of cocktails
+Add cocktails to a favorites list
+Remove cocktails from a favorites list
+Search for new cocktails by ingredient
+Apply styling to the page
+Your application should consist of multiple modular React components. Use what you have learned (React component lifecycles, when/how to send HTTP requests, passing props, prop drilling, lifting state, and handling events) to complete this project. Below is a list of API endpoints you can use to retrieve data from the server:
+
+GET http://localhost:3001/ returns an object with property drinks that is an array of drink objects
+GET http://localhost:3001/search/{Your ingredient search query here} returns an object with property drinks that is an array of drink objects that match the search query.
+Stretch Goals
+Create an "Add a Drink" function that adds a drink to your list. This drink should be stored "in-memory", so changes should not be posted to the API.
+Refactor code to utilize React Hooks
+Implement testing using the React Testing Library
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,153 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function Cocktail({ drink, onCocktailClick }) {
+  return (
+    <div
+      className="cocktail"
+      onClick={() => {
+        onCocktailClick(drink);
+      }}
+    >
+      <img src={drink.strDrinkThumb}></img>
+      <p>{drink.strDrink}</p>
+    </div>
+  );
+}
+
+function Cocktails({ cocktailList, clickHandler }) {
+  return (
+    <div>
+      {cocktailList.map((drink) => {
+        return <Cocktail onCocktailClick={clickHandler} drink={drink} />;
+      })}
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cocktailList, setCocktailList] = useState([
+    { strDrink: "Fetching drinks...", strDrinkThumb: null },
+  ]);
+  const [favorites, setFavorites] = useState([
+    { strDrink: "No favorites added.", strDrinkThumb: null },
+  ]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/")
+      .then((httpResponse) => httpResponse.json())
+      .then((data) => {
+        setCocktailList(data.drinks.slice(0, 10));
+      });
+  }, []);
+
+  const addCocktail = (drink) => {
+    let cocktails = favorites.map((drink) => drink);
+    if (cocktails[0].strDrink === "No favorites added.") {
+      cocktails.splice(0, 1, drink);
+    } else if (
+      cocktails.filter((cocktail) => cocktail.strDrink == drink.strDrink)
+        .length < 1
+    ) {
+      cocktails.push(drink);
+    }
+
+    setFavorites(cocktails);
+  };
+
+  const removeCocktail = (drink) => {
+    // TODO
+    let removeCocktails = favorites.filter(
+      (cocktail) => cocktail.strDrink != drink.strDrink
+    );
+    if (removeCocktails.length < 1) {
+      removeCocktails.push({
+        strDrink: "No favorites added.",
+        strDrinkThumb: null,
+      });
+    }
+    setFavorites(removeCocktails);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Cocktails
+        className="cocktail-list favorites"
+        cocktailList={favorites}
+        clickHandler={removeCocktail}
+      />
+      <Cocktails
+        className="cocktail-list results"
+        cocktailList={cocktailList}
+        clickHandler={addCocktail}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+/*
+ Render a default list of cocktails
+  • useState(["Fetching cocktails"]), cocktailsList, setCocktailsList
+  • useEffect fetch http://localhost:3001/ then grab top 10 drinks in object <- we can make it random
+
+ Favorites list
+  • useState([]), favoriteCocktails setFavoriteCocktails
+
+ Add cocktails to a favorites list
+  • setFavoriteCocktails with new data
+
+ Remove cocktails from favorites list
+  • setFavoriteCocktails with spliced data
+
+ Search by ingrediant
+  • useEffect with a fetch to http://localhost:3001/search/{item}
+
+ Styling
+  • use className and apply styles
+*/
+
+/*
+
+.map <- method of the Array class
+
+.map( callback Function )
+  • it passes 3 arguments into the callback function: element, index, collection
+  • it creates a new array
+  • then applies the callback function to every element in the array
+  • then pushes the result of the callback function into the new array
+  • then return the new array
+  
+    () => {} <- this function technically has 3 arguments being passed into it
+    (element) => {} <- do something with the element 
+    (element, index) => {}
+    (element, index, collection) => {}
+
+    .map will always pass in the element, then index, then collection
+    ---- element is the value of the current index location in the array
+    ---- index is the current index location in the array
+    ---- collection is the current array
+
+      // -- What .map does --
+      .map invoked on an array with a callback function
+        let result = [];
+        for each element in array
+          results.push(callback function(element, index, collection))
+        return result;
+      end of .map
+      // -------------------------
+
+    // Example
+    let myArray = [0,1,2,7,4]
+    let myCb = (number, index, collection) => {
+      let value = number + index + collection[index]
+      return value == 0 ? "zero" : value;
+    }
+
+    myArray.map(myCb) -> ["zero",3,6,17,12]
+
+    // ---------------------------------
+    
+
+*/
